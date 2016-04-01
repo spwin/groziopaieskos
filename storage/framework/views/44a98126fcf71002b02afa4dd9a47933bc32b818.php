401e97f6604de241ab61530a3a7ca113b9be61f8
Stanislav <stanislav.markevic@gmail.com>
2016-04-01 09:43:18 -0400

    remved unused files

--- storage/framework/views/44a98126fcf71002b02afa4dd9a47933bc32b818.php
+++
@@ -1,7 +0,0 @@
-<?php $__env->startSection('content'); ?>
-    <?php echo $__env->make('admin.navigation', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
-    <?php foreach($cities as $city): ?>
-        <?php echo e($city->name); ?> <br/>
-    <?php endforeach; ?>
-<?php $__env->stopSection(); ?>
-<?php echo $__env->make('admin', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
