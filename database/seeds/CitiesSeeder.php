b08b54d5d43c12e27276c35a25b1f51849315e31
paulmark15 <pavel.markevic1@gmail.com>
2016-04-03 23:06:40 +0300

    alytus example

--- database/seeds/CitiesSeeder.php
+++ database/seeds/CitiesSeeder.php
@@ -16,18 +16,84 @@ class CitiesSeeder extends Seeder
 
         $regions = [
             'vilnius' => [
-                'vilnius' => 'Vilnius',
-                'nemencine' => 'Nemenčinė'
+                'vilnius-miestas' => 'Vilnius',
+                'vilniaus-raj' => 'Vilniaus raj.',
+                'svencioniu-raj' => 'Švenčionių raj.',
+                'ukmerges-raj' => 'Ukmergės raj.',
+                'sirvintu-raj' => 'Širvintų raj.',
+                'elektrenu-sav' => 'Elektrėnų sav.',
+                'traku-raj' => 'Trakų raj.',
+                'salcininku-raj' => 'Šalčininkų raj.'
             ],
-            'kaunas' => [],
-            'siauliai' => [],
-            'klaipeda' => [],
-            'panevezys' => [],
-            'utena' => [],
-            'alytus' => [],
-            'telsiai' => [],
-            'marijampole' => [],
-            'taurage' => [],
+            'kaunas' => [
+                'kaunas-miestas' => 'Kaunas',
+                'kauno-raj' => 'Kauno raj.',
+                'raseiniu-raj' => 'Raseinių raj.',
+                'kedainiu-raj' => 'Kėdainių raj.',
+                'jonavos-raj' => 'Jonavos raj.',
+                'kaisiadoriu-raj' => 'Kaišiadorių raj.',
+                'prienu-raj' => 'Prienų raj.',
+                'birstono-sav' => 'Birštono sav.'
+            ],
+            'siauliai' => [
+                'siauliai-miestas' => 'Šiauliai',
+                'siauliai-raj' => 'Šiauliai raj.',
+                'akmenes-raj' => 'Akmenės raj.',
+                'joniskio-raj' => 'Joniškio raj.',
+                'pakruojo-raj' => 'Pakruojo raj.',
+                'radviliskio-raj' => 'Radviliškio raj.',
+                'kelmes-raj' => 'Kelmės raj.'
+            ],
+            'klaipeda' => [
+                'klaipeda-miestas' => 'Klaipėda',
+                'klaipedos-raj' => 'Klaipėdos raj.',
+                'kretingos-raj' => 'Kretingos raj.',
+                'palangos-sav' => 'Palangos raj.',
+                'skuodo-raj' => 'Skuodo raj.',
+                'neringos-sav' => 'Neringos sav.',
+                'silutes-raj' => 'Šilutės raj.'
+            ],
+            'panevezys' => [
+                'panevezys-city' => 'Panevėžys',
+                'panevezio-raj' => 'Panevėžio raj.',
+                'pasvalio-raj' => 'Pasvalio raj.',
+                'birzu-raj' => 'Biržų raj.',
+                'kupiskio-raj' => 'Kupiškio raj.',
+                'rokiskio-raj' => 'Rokiškio raj.'
+            ],
+            'utena' => [
+                'utenos-raj' => 'Utenos raj.',
+                'anyksciu-raj' => 'Anykščių raj.',
+                'moletu-raj' => 'Molėtų raj.',
+                'zarasu-raj' => 'Zarasų raj.',
+                'ignalinos-raj' => 'Ignalinos raj.'
+            ],
+            'alytus' => [
+                'alytus-miestas' => 'Alytus',
+                'alytaus-raj' => 'Alytaus raj.',
+                'lazdiju-raj' => 'Lazdijų raj.',
+                'druskininku-sav' => 'Druskininkų sav.',
+                'varenos-raj' => 'Varėnos raj.'
+            ],
+            'telsiai' => [
+                'mazeikiu-raj' => 'Mažeikių raj.',
+                'telsiu-raj' => 'Telšių raj.',
+                'plunges-raj' => 'Plungės raj.',
+                'rietavo-raj' => 'Rietavo raj.'
+            ],
+            'marijampole' => [
+                'marijampoles-sav' => 'Marijampolės sav.',
+                'kalvarijos-sav' => 'Kalvarijos sav.',
+                'vilkaviskio-raj' => 'Vilkaviškio raj.',
+                'kazlu-rudos-sav' => 'Kazlų Rūdos sav.',
+                'sakiu-raj' => 'Šakių raj.'
+            ],
+            'taurage' => [
+                'taurages-raj' => 'Tauragės raj.',
+                'silales-raj' => 'Šilalės raj.',
+                'jurbarko-raj' => 'Jurbarko raj.',
+                'pagegiu-sav' => 'Pagėgių sav.'
+            ],
         ];
 
         foreach($regions as $slug => $cities) {
